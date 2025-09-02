fe63ef6ff88f17e59c7e33c447c486df563c967d
Olga Zotova <olgazotova1976@yandex.ru>
2025-09-02 13:23:24 +0300

    Run the test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,10 @@
 public class Main {
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
+        long amount = 50000;
+        boolean registered = false;
+        long bonus = service.calculate(amount, registered);
+        System.out.println("Бонус: " + bonus);
+    }
 }
+
